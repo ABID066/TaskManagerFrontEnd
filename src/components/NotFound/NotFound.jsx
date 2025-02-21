42c7b13f9c8747115a043bffb3ebed7a29564d42
ABID066 <mdabid1152@gmail.com>
2025-02-21 14:29:40 +0600

    updating

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     let navigate = useNavigate();
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <div className="flex text-center flex-col items-center justify-center h-screen bg-gray-100">
             <h1 className="text-9xl font-extrabold text-[#cb0c9f]">404</h1>
             <h2 className="text-3xl font-bold text-gray-800 mt-4">Oops! Page Not Found</h2>
             <p className="text-gray-600 mt-2 text-center max-w-lg">
@@ -13,7 +13,7 @@ const NotFound = () => {
             </p>
             <button
                 onClick={() => navigate("/")}
-                className="mt-6 px-6 py-3 bg-[#cb0c9f] text-white text-lg font-medium rounded-full shadow-lg hover:bg-[#a80a7f] transition-all duration-300"
+                className="mt-6 px-6 py-3 btn-primary text-white text-lg font-medium rounded-full shadow-lg hover:bg-[#a80a7f] transition-all duration-300"
             >
                 Go Back Home
             </button>
